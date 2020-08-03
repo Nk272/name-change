2ec7d3cfc9938a573a0753dcb16c14db47c019e4
Nk272 <43395018+Nk272@users.noreply.github.com>
2020-08-03 15:15:44 +0530

    Delete convert.py

--- convert.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-filename="Final Data for portal.xlsx"
-dbname="parts"
-con=sqlite3.connect(dbname+".db")
-df=pd.read_excel(filename,sheet='Sheet1')
-df.to_sql("Participants",con,index=False,if_exists="replace")
-con.commit()
-con.close()
