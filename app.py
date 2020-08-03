4d5ea8a38c9cfbcca6a8bc6e4295a2af60bdd959
Nk272 <43395018+Nk272@users.noreply.github.com>
2020-08-03 17:13:49 +0530

    corrected

--- app.py
+++ app.py
@@ -16,34 +16,37 @@ participants=Base.classes.Participants
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    person=None
+    person="emp"
     if request.form:
         try:
             mail=request.form.get("email")
+            mail=mail.lower()
             person = db.session.query(participants).filter_by(Email=mail).first()
+            print(person)
             db.session.commit()
         except Exception as e:
-            person=("You have not given exam, please check with organisers")
+            person=("Error, please check with organisers")
             print(e)
     return render_template("home.html",perso=person)
 
 
 @app.route('/namechange', methods=["GET", "POST"])
 def change ():
-    person=None
+    person="emp"
     if request.form:
         try:
             mail=request.form.get("email")
+            mail=mail.lower()
             person = db.session.query(participants).filter_by(Email=mail).first()
-            
             name1=request.form.get("name1")
             name2=request.form.get("name2")
-            
-            if (name1==name2 and name1!="" and name2!=""):
+            if (name1!=name2 or name1=="" or name2==""):
+                person="diff"
+            if (name1==name2 and name1!="" and name2!="" and person!=None):
                 person.Name=name2
             db.session.commit()
         except Exception as e:
-            person=("You have not given exam, please check with organisers")
+            person=("Error, please check with organisers")
             print(e)
     return render_template("namechange.html",perso=person)
 
